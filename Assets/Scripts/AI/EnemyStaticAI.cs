f1bb393b1502760d9075ef8941bd76a9089fbf04
Mobile PC <tahri.e@gmail.com>
2018-01-27 07:35:23 -0500

    Updates to AI scripts with triggers
    Patrolling AI

--- Assets/Scripts/AI/EnemyStaticAI.cs
+++ Assets/Scripts/AI/EnemyStaticAI.cs
@@ -1,65 +1,42 @@
-﻿// Patrol.cs
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using System.Collections;
 
-
-public class EnemyPatrol : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    
-    public Transform[] points; // locations to patrol
-    private int destPoint = 0;
+
+    public Transform destination;
+    public Transform startPos;
     private float agroRange = 15f;
     private float minAttackRange = 3f;
     private float fov = 90f;
     private NavMeshAgent agent;
- 
-    bool agro = false;
+    bool agro;
+
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-       // points[0].position = agent.transform.position;
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
-        agent.autoBraking = false;
+        agent = gameObject.GetComponent<NavMeshAgent>();
 
-        GotoNextPoint();
     }
 
+    void attack()
+    {
+        //face player
+        //agent.SetDestination = player.
+        if (agent.remainingDistance < minAttackRange)
+        {
+            // stop moving to attack
+        }
+    }
 
     void GotoNextPoint()
     {
-        //agent = GetComponent<NavMeshAgent>();
-        // Rds(points.Length == 0)
-        //return;
+        // Set the agent to go to the start position.
+        agent.destination = startPos.transform.position;
+    }
 
-        // Set the agent to go to the currently selected destination.
-        // agent.destination = points[destPoint].position;
-        Vector3 position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
-        agent.destination = position;
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        //destPoint = (destPoint + 1) % points.Length;
-    }
-    
-    void attack()
-    {
-        agro = true;
-        //face player
-        //agent.SetDestination = player.
-        if (agent.remainingDistance < minAttackRange )
-        {
-          // stop to attack
-        }
-        else if(agent.remainingDistance > agroRange)
-        {
-            agro = false;
-            // goes to next way point 
-        }
-        //if range attack player
-    }
     void OnTriggerEnter(Collider other)
     {//This is to see if the player is within a certain distance of the AI.
         if (other.gameObject.tag == "Player")
@@ -77,7 +54,7 @@ public class EnemyPatrol : MonoBehaviour
                 {
                     if (hit.collider.tag == "Player")
                     {
-                        agro = true; //AI Character sees the player
+                       agro = true; //AI Character sees the player
                         attack();
                         //DoWaypoints = false; //don't walk waypoints anymore
                     }
@@ -110,15 +87,19 @@ public class EnemyPatrol : MonoBehaviour
     }
 
     void Update()
-    {      
+    {
+
         // Choose the next destination point when the agent gets
         // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f && !agro)
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
-            GotoNextPoint();
+            agent.destination = startPos.transform.position;
         }
-   
-
+        else
+        {
+            attack();
+        }
+        
     }
-}
 
+}
