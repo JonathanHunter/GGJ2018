8f9e9a6c16954f7a3ea48132e3873eb971fbdbd9
nshooter3 <nick.shooter@gmail.com>
2018-01-27 14:27:55 -0500

    UI juicing stuff

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour {
+
+    public Image[] hearts, emptyHearts;
+    private int maxHearts = 3, currentHearts = 3, prevHearts = 3;
+    private float bigPulseTimer = 0, maxBigPulseTimer = 0.25f;
+
+    Vector3 initScale;
+
+    void Start()
+    {
+        initScale = transform.localScale;
+    }
 
     /// <summary>
     /// Call this at the start of the scene, and if the player's max health ever changes
     /// </summary>
     /// <param name="maxHealth"></param>
-    public void SetMaxHealth(int maxHealth)
+    public void SetMaxHealth(int maxHearts)
     {
-
+        this.maxHearts = maxHearts;
+        currentHearts = maxHearts;
+        prevHearts = maxHearts;
     }
 
     /// <summary>
@@ -19,7 +33,8 @@ public class HealthUI : MonoBehaviour {
     /// <param name="damage"></param>
     public void TakeDamage(int damage)
     {
-
+        currentHearts = Mathf.Max(currentHearts - damage, 0);
+        UpdateUI();
     }
 
     /// <summary>
@@ -28,6 +43,49 @@ public class HealthUI : MonoBehaviour {
     /// <param name="points"></param>
     public void Heal(int points)
     {
+        currentHearts = Mathf.Min(currentHearts + points, maxHearts);
+        UpdateUI();
+    }
 
+    void UpdateUI()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i >= currentHearts)
+            {
+                emptyHearts[i].enabled = true;
+                hearts[i].enabled = false;
+            }
+            else
+            {
+                emptyHearts[i].enabled = false;
+                hearts[i].enabled = true;
+            }
+        }
+        if (prevHearts != currentHearts)
+        {
+            bigPulseTimer = maxBigPulseTimer;
+        }
+        prevHearts = currentHearts;
+    }
+
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Q)){
+            TakeDamage(1);
+        }
+        if (Input.GetKeyDown(KeyCode.E)){
+            Heal(1);
+        }*/
+
+        if (bigPulseTimer > 0)
+        {
+            transform.localScale = Vector3.Lerp(initScale, initScale * 1.25f, bigPulseTimer/maxBigPulseTimer);
+            bigPulseTimer -= Time.deltaTime;
+            if (bigPulseTimer <= 0)
+            {
+                transform.localScale = initScale;
+            }
+        }
     }
 }
--- Assets/Scripts/UI/ReloadUI.cs
+++ Assets/Scripts/UI/ReloadUI.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReloadUI : MonoBehaviour {
+
+    private int bulletsInClip, maxClipSize = 6;
+    private float rechargeTimer, toggleFlashTimer;
 
     /// <summary>
     /// Whether or not the Reload is recharging
@@ -14,8 +18,33 @@ public class ReloadUI : MonoBehaviour {
     /// </summary>
     /// <param name="duration"></param>
     /// How long the reload will take
-    public void StartRecharge(float duration)
+    public void StartRecharge()
     {
+        rechargeTimer = 3.0f;
+        toggleFlashTimer = 0.5f;
+        IsRecharging = true;
+    }
 
+    /// <summary>
+    /// Used to update UI values
+    /// </summary>
+    /// <param name="bulletsInClip"></param>
+    /// Number of bullets in your clip
+    public void UpdateValues(int bulletsInClip)
+    {
+        this.bulletsInClip = bulletsInClip;
+    }
+
+    void Update()
+    {
+        //if = 
+        if (rechargeTimer > 0)
+        {
+            rechargeTimer -= Time.deltaTime;
+            if (rechargeTimer <= 0)
+            {
+                IsRecharging = false;
+            }
+        }
     }
 }
