01dc8bcd85bd89c06f3eed1268597ec68b0a7564
nshooter3 <nick.shooter@gmail.com>
2018-01-27 09:38:56 -0500

    added framework for UI presentation

---
+++ Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour {
+
+    /// <summary>
+    /// Call this at the start of the scene, and if the player's max health ever changes
+    /// </summary>
+    /// <param name="maxHealth"></param>
+    public void SetMaxHealth(int maxHealth)
+    {
+
+    }
+
+    /// <summary>
+    /// Use this to update the UI if the player takes damage
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(int damage)
+    {
+
+    }
+
+    /// <summary>
+    /// Use this to update the UI if the player regains some health
+    /// </summary>
+    /// <param name="points"></param>
+    public void Heal(int points)
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/ScreechUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreechUI : MonoBehaviour {
+
+    /// <summary>
+    /// Whether or not the Reload is recharging
+    /// </summary>
+    public bool IsRecharging { get; private set; }
+
+    /// <summary>
+    /// Call this to kick off the screech recharge presentation
+    /// </summary>
+    /// <param name="duration"></param>
+    /// How long the reload will take
+    public void StartRecharge()
+    {
+
+    }
+}
