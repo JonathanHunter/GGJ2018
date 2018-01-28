f34b39f4494a1699547f781bac8fa64e2ad36980
Jonathan Hunter <hunterjonathant@gmail.com>
2018-01-28 10:51:32 -0500

    started boss

--- Assets/Scripts/Character/Enemy/EnemyBoss.cs
+++ Assets/Scripts/Character/Enemy/EnemyBoss.cs
@@ -4,24 +4,93 @@
 
     public class EnemyBoss : Enemy
     {
+        public Transform[] RunLocations;
+        public Collider melee;
+
+        private enum State { Chase, Attack, Run, Stunned }
+
+        private State prevState;
+        private State currState;
+        private int attackHash;
+        private int stunHash;
+        private bool attackDone;
+        private bool doOnce;
 
         protected override void LocalInit()
         {
-            throw new System.NotImplementedException();
+            this.currState = State.Chase;
+            this.prevState = State.Chase;
+            this.attackHash = Animator.StringToHash("Attack");
+            this.stunHash = Animator.StringToHash("Stun");
+            this.attackDone = false;
+            this.doOnce = false;
         }
 
         protected override void LocalUpdate()
         {
-            throw new System.NotImplementedException();
+            switch (this.currState)
+            {
+                case State.Chase: Chase(); break;
+                case State.Attack: Attack(); break;
+                case State.Run: Run(); break;
+                case State.Stunned: Stunned(); break;
+            }
+
+            if(this.currState != this.prevState)
+            {
+                this.prevState = currState;
+                this.attackDone = false;
+                this.doOnce = false;
+            }
         }
 
         protected override void TookDamage()
         {
-            throw new System.NotImplementedException();
         }
 
         protected override void InAgroRange()
         {
+        }
+
+        public void AttackDone()
+        {
+            this.attackDone = true;
+        }
+
+        public void StunDone()
+        {
+
+        }
+
+        private void Chase()
+        {
+
+        }
+
+        private void Attack()
+        {
+            if (!this.doOnce)
+            {
+                this.melee.enabled = true;
+                this.doOnce = true;
+                this.enemyAnimator.SetTrigger(this.attackHash);
+            }
+
+            if(this.attackDone)
+            {
+                this.melee.enabled = false;
+                this.currState = State.Stunned;
+            }
+        }
+
+        private void Run()
+        {
+
+        }
+
+        private void Stunned()
+        {
+
         }
     }
 }
