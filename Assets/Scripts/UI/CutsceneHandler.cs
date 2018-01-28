0996276e14c87040bd39335769f7c2382d3c3c4a
nshooter3 <nick.shooter@gmail.com>
2018-01-28 12:59:42 -0500

    added credits to end of last cutscene

--- Assets/Scripts/UI/CutsceneHandler.cs
+++ Assets/Scripts/UI/CutsceneHandler.cs
@@ -11,6 +11,7 @@ public class CutsceneHandler : MonoBehaviour
 {
 
     public CutsceneObject[] cutsceneObjects1, cutsceneObjects2, cutsceneObjects3, cutsceneObjects4;
+    public Text credits;
     public int cutsceneVersion = 1;
 
     private CutsceneObject[] curObj;
@@ -59,7 +60,7 @@ public class CutsceneHandler : MonoBehaviour
                         UnityEngine.SceneManagement.SceneManager.LoadScene("CityHall");
                         break;
                     case 4:
-                        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+                        StartCoroutine(ShowCredits());
                         break;
                 }
                 inactive = true;
@@ -69,5 +70,27 @@ public class CutsceneHandler : MonoBehaviour
         {
             curObj[index].Activate();
         }
+    }
+
+    IEnumerator ShowCredits()
+    {
+        yield return new WaitForSeconds(1.0f);
+        float dur = 2.0f;
+        Color initCol = credits.color, finalCol = new Color(credits.color.r, credits.color.g, credits.color.b, 1);
+        for (float d = dur; d > 0; d -= Time.deltaTime)
+        {
+            credits.color = Color.Lerp(finalCol, initCol, d / dur);
+            yield return new WaitForSeconds(0f);
+        }
+        credits.color = finalCol;
+        yield return new WaitForSeconds(4.0f);
+        for (float d = dur; d > 0; d -= Time.deltaTime)
+        {
+            credits.color = Color.Lerp(initCol, finalCol, d / dur);
+            yield return new WaitForSeconds(0f);
+        }
+        credits.color = initCol;
+        yield return new WaitForSeconds(1.0f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
     }
 }
