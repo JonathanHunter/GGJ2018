21ec116645fb5eb53b89479bf353bdab9f6704c8
Spex130 <Gamerguy56@bellsouth.net>
2018-01-28 13:03:09 -0500

    Exploding Desk!
    It blows up.

---
+++ Assets/Scripts/Effects/ExplodingDesk/ExplodingDesk.cs
@@ -0,0 +1,61 @@
+﻿namespace GGJ2018.Scripts.Effects.ExplodingDesk
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Util;
+
+    public class ExplodingDesk : MonoBehaviour
+    {
+
+        public GameObject desk;
+        public GameObject explodedDesk;
+        public SoundPlayer myAudio;
+        public bool isComplete = false;
+        private bool sfPlayed = false;
+
+        private float soundTimer = 3f;
+
+        // Use this for initialization
+        void Start()
+        {
+            desk.SetActive(true);
+            explodedDesk.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isComplete == false)
+            {
+                desk.SetActive(true);
+                explodedDesk.SetActive(false);
+            }
+            else
+            {
+                if (sfPlayed == false)
+                {
+                    Destroy(desk);
+                    explodedDesk.SetActive(true);
+                    explodedDesk.transform.DetachChildren();
+                    Destroy(explodedDesk);
+                    sfPlayed = true;
+                    myAudio.PlaySong(0);
+                }
+            }
+
+            if (sfPlayed){
+                soundTimer -= Time.deltaTime;
+                if(soundTimer <= 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        public void explode()
+        {
+            isComplete = true;
+        }
+    }
+}
