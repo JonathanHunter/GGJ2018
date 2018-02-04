5204a109c0f412e552cce3ee5d4767114bfa9cb9
Mitch McClellan <mmcclellan7@gatech.edu>
2018-02-03 19:47:11 -0500

    Tutorial update (#6)
    * First prefab set up and input images imported
    
    * Adding rest of button prefabs
    - Hotswapping working from mouse+keyboard to xbox
    - Rearranged tutorial canvases in Cavern scene
    
    * Removed print statements

---
+++ Assets/Scripts/UI/InputImageHotSwap.cs
@@ -0,0 +1,59 @@
+﻿namespace GGJ2018.UI
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+	using Util;
+
+	enum ActionType {Movement, Aim, Shooting, Screech, Meelee, Reload}
+
+	public class InputImageHotSwap : MonoBehaviour
+	{
+		[SerializeField]
+		Sprite[] images;
+		[SerializeField]
+		ActionType type;
+
+		private SpriteRenderer image;
+		private bool usingPad;
+
+		void Start ()
+		{
+			image = GetComponentInChildren<SpriteRenderer>();
+			usingPad = CustomInput.UsingPad;
+			if (CustomInput.UsingPad) 
+			{
+				SetImage(1);
+			}
+		}
+		
+		void Update ()
+		{
+			if (CustomInput.UsingPad != usingPad)
+			{
+				usingPad = CustomInput.UsingPad;
+				if (usingPad)
+				{
+					if (!(type == ActionType.Screech || type == ActionType.Reload))
+						SetImage(1);	// General parts of a controller
+					else if (CustomInput.GamePadType(0) == CustomInput.ControlType.Xbox)
+						SetImage(1);	// Xbox buttons
+					else if (CustomInput.GamePadType(0) == CustomInput.ControlType.PS3
+						  || CustomInput.GamePadType(0) == CustomInput.ControlType.PS4)
+						SetImage(2);	// PS buttons
+				} 
+				else
+				{
+					SetImage(0);		// Keyboard and Mouse
+				}
+			}
+		}
+
+		void SetImage(int i) 
+		{
+			image.sprite = null;
+			image.sprite = images[i];
+		}
+	}
+}
