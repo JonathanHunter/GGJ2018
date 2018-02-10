0780f5a813da448989c5646476185f347a57c655
Jonathan Hunter <hunterjonathant@gmail.com>
2018-02-09 21:41:48 -0500

    Sonar Generator (#13)

---
+++ Assets/Scripts/Effects/SonarGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace GGJ2018.Effects
+{
+    using UnityEngine;
+    using ObjectPooling;
+
+    public class SonarGenerator : MonoBehaviour
+    {
+        public Transform sonarPoint;
+        public float sonarRate;
+        public float sonarSpeed;
+        public float sonarSize;
+
+        private float sonarTimer;
+
+        private void Start()
+        {
+            this.sonarTimer = sonarRate;
+        }
+
+        private void Update()
+        {
+            if (Managers.GameState.Instance.CurrentState != Managers.GameState.State.Playing)
+                return;
+
+            if((this.sonarTimer -= Time.deltaTime) < 0)
+            {
+                GameObject s = SonarPool.Instance.GetSonar(this.sonarSpeed, this.sonarSize);
+                if (s != null)
+                    s.transform.position = this.sonarPoint.position;
+
+                this.sonarTimer = this.sonarRate;
+            }
+        }
+    }
+}
